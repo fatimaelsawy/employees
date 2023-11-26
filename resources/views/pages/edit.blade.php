372ad93ed63f7fdaf59efdecfb47156e30435e95
fatimaelsawy <146548596+fatimaelsawy@users.noreply.github.com>
2023-11-26 17:04:20 +0200

    Add files via upload

---
+++ resources/views/pages/edit.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.master')
+
+@section('title', 'Add Employee Page')
+
+@section('main')
+    <div class="alert alert-primary text-center
+     py-3 my-5 container w-50 fs-4">
+        Add New Employee
+    </div>
+
+    <div class="container my-5 w-50 mx-auto">
+        <form action={{'/update-employee/'. $employee->id}} method="POST">
+            @csrf
+            @method('PUT')
+            <div class="form-group my-3">
+                <label for="first_name">First Name</label>
+                <input type="text" class="form-control" id="first-name" name="first_name" value={{$employee->first_name}}>
+            </div>
+
+            <div class="form-group my-3">
+                <label for="last_name">Last Name</label>
+                <input type="text" class="form-control" id="last-name" name="last_name" value={{$employee->last_name}}>
+            </div>
+
+            <div class="form-group my-3">
+                <label for="job">Job</label>
+                <input type="text" class="form-control" id="job" name="job" value={{$employee->job}}>
+            </div>
+            <div class="form-group my-3">
+                <label for="phone_number">Phone Number</label>
+                <input type="text" class="form-control" id="phone_number" name="phone_number" value={{$employee->phone_number}}>
+            </div>
+
+            <div class="form-group my-3">
+                <label for="salary">Salary</label>
+                <input type="number" class="form-control" id="salary" name="salary" value={{$employee->salary}}>
+            </div>
+
+            <div class="form-group my-3">
+                <label for="desc">Gender</label>
+                <input type="text" class="form-control" id="gender" name="gender" value={{$employee->gender}}></input>
+            </div>
+
+            <input type="submit" value="Add Employee" class="btn btn-dark my-2">
+        </form>
+    </div>
+@endsection
